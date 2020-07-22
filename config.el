ea3bd04bca28cbae1dffb520b1aa290d4d8cbb02
Chris Callan <chriscallanjr@me.com>
2020-07-21 21:14:57 -0400

    adds window split preview

--- config.el
+++ config.el
@@ -22,7 +22,7 @@
 ;; font string. You generally only need these two:
 ;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
 ;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
-(setq doom-font (font-spec :family "Input Mono" :size 20))
+(setq doom-font (font-spec :family "Input Mono" :size 16))
 
 (setq treemacs-position 'right)
 
@@ -35,11 +35,20 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-tomorrow-day)
+(setq doom-theme 'doom-tomorrow-night)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type 'relative)
+
+(setq evil-vsplit-window-right t
+      evil-split-window-below t)
+
+(defadvice! prompt-for-buffer (&rest _)
+  :after '(evil-window-split evil-window-vsplit)
+  (+ivy/switch-buffer))
+
+(setq +ivy-buffer-preview t)
 
 (map!
   :n "C-h" #'evil-window-left
